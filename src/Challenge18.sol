9ab0c26e05ae111ce29f90aefb0d6f250ff2d219
RolandMtsariashvili <rolandmcariashvili@gmail.com>
2025-09-10 19:40:08 +0400

    Challenge18 completed

--- src/Challenge18.sol
+++ src/Challenge18.sol
@@ -125,6 +125,7 @@ contract Challenge18 {
     function _mint(address account, uint256 value) internal {
         require(account != address(0), "ERC20: mint to the zero address");
 
+        _totalSupply += value;
         unchecked {
             _balances[account] += value;
         }
